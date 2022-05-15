1448b094b3d968ab3ed594bf7a1ea979701f1d57
paradroid-x <72254193+paradroid-x@users.noreply.github.com>
2022-05-15 20:57:40 +0200

    completed base AJAX function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-
 let w;
 
 function mostraProdotto(prod){
     const xhr = new XMLHttpRequest();
     xhr.onload = function () {
-        var res = new Object();
-        res = JSON.parse(this.response);
         
-        if(res.products[prod].quantity == "0"){ //se non disponibili va a finire qui
-            document.getElementById("body-wrapper").innerHTML = "NON DISPONIBILE";
-        } else {
-            document.getElementById("body-wrapper").innerHTML = res.products[prod].type + " --> " +  res.products[prod].quantity;
+        if(this.readyState == 4 && this.status == 200){ //check della connessione
+            var res = new Object();
+            var i = parseInt(prod) + 1; //questo schifo serve per ottenere l'immagine giusta
+            res = JSON.parse(this.response);
+
+            document.getElementById("prd-img").src = "img/" + i + ".jpg";
+            if(res.products[prod].quantity == "0"){ //se non disponibili
+                document.getElementById("prd-dsc").innerHTML = "NON DISPONIBILE";
+            } else {
+                document.getElementById("prd-dsc").innerHTML = res.products[prod].type + " --> " +  res.products[prod].quantity;
+            } 
+
         }
     }
     xhr.open("GET", "db/db.json", true);
